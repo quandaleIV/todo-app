2f59f5645833d62e171ecce6b45d7541cc30792a
quandaleIV <167491188+quandaleIV@users.noreply.github.com>
2025-09-13 12:04:32 +1000

    erorr fixed

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { useState, useEffect } from "react";
 import { Header } from "./components/Header";
 import { Tabs } from "./components/Tabs";
-import { TodoInput } from "./components/Todoinput";
+import { TodoInput } from "./components/TodoInput";
 import { TodoList } from "./components/TodoList";
 
 
